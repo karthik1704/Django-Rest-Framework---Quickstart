8908812189d9fc952bdadf069b6172b295006ebb
Karthik A <karthikthee7@gmail.com>
2020-01-03 13:16:34 +0530

    Quickstart from Django  Rest framework website

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User,Group
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields=[
+            'id',
+            'username',
+            'email',
+            'is_staff',
+            ]
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model  = Group
+        fields = [
+            'url',
+            'name',
+        ]
